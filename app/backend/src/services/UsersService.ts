e36812ead93a451515c2f52f4dea951bdac38cb3
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2023-11-03 14:26:16 -0300

    req 8

--- app/backend/src/services/UsersService.ts
+++ app/backend/src/services/UsersService.ts
@@ -1,3 +1,4 @@
+import bcrypt = require('bcryptjs');
 import UsersModel from '../database/models/UsersModel';
 import users from '../Interfaces/interUsers';
 import { ServiceResponse } from '../Interfaces/ServiceResponse';
@@ -11,10 +12,13 @@ type Body = {
 export default class UsersService {
   private model = UsersModel;
 
-  public async login(body: Body): Promise<ServiceResponse<users[]>> {
+  public async login(body: Body): Promise<ServiceResponse<object | null>> {
     const error1 = await validate.validateBodyLogin(body);
     if (error1) return { status: error1.status, data: { message: error1.message } };
-    const allTeams = await this.model.findAll();
-    return { status: 200, data: allTeams };
+    const foundUser = await this.model.findOne({ where: { email: body.email } });
+    if (!foundUser || !bcrypt.compareSync(body.password, foundUser.dataValues.password)) {
+      return { status: 400, data: { message: 'All fields must be filled' } };
+    }
+    return { status: 200, data: foundUser };
   }
 }
